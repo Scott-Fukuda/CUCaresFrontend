4b46b9c3067d261a49211b3ed9eba15581db3691
Scott Fukuda <sdf72@cornell.edu>
2025-09-30 13:13:40 -0400

    added terms of service

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -163,6 +163,18 @@ const AboutUsPage: React.FC = () => {
             </button>
           </div>
         </section>
+        <p className="text-xs text-gray-500 mt-6 text-center">
+          Click here to see our {" "}
+          <a
+            href="/terms_of_service.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline hover:text-gray-700"
+          >
+            Terms of Service and Privacy Policy
+          </a>
+          .
+        </p>
       </div>
     </div>
   );
